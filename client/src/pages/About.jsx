653eb8c583231500b03163ee86bcc0c5f7fa0c0a
goodmindsg <76161488+goodmindsg@users.noreply.github.com>
2023-12-03 03:38:57 +0530

    complete about and home page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Good Villa</h1>
+      <p className='mb-4 text-slate-700'>Good Villa stands out as a premier real estate agency, focusing on assisting clients in purchasing, selling, and renting properties in highly sought-after neighborhoods. Our team of seasoned agents is committed to delivering outstanding service, ensuring a seamless experience throughout the buying and selling journey.</p>
+      <p className='mb-4 text-slate-700'>
+      We are dedicated to empowering our clients to reach their real estate aspirations through expert guidance, personalized service, and a profound comprehension of the local market. Whether you're seeking to purchase, sell, or rent a property, we are committed to assisting you at every stage of the process.
+      </p>
+      <p className='mb-4 text-slate-700'>With a team boasting extensive experience and knowledge in the real estate industry, we are unwaveringly dedicated to delivering the utmost level of service to our clients. We firmly believe that the process of buying or selling a property should be an exciting and rewarding journey. Our commitment is to turn this belief into a reality for each and every one of our clients.</p>
+    </div>
   )
 }
